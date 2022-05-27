5f1afbb0ccfc730a1e11f0e2d881c65075abcd74
Ashwini Rathod <rashwini510@gmail.com>
2022-05-27 18:34:32 +0530

    Added:Power of Two

--- src/FlipCoin.java
+++ src/FlipCoin.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-4public class FlipCoin {
+public class FlipCoin {
 
         public static void main(String[] args) {
             System.out.println("Give a number: ");
